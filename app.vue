138a0b61f7060d7e7b4779e4f888c856f3bcdfd2
Nico Diaz <nicodiazan@gmail.com>
2025-04-14 15:12:22 +0200

    Make the looking glass hidden in production

--- app.vue
+++ app.vue
@@ -1,6 +1,10 @@
+<script setup>
+const isProduction = process.env.NODE_ENV === "production";
+</script>
+
 <template>
   <div>
-    <DevLookingGlass />
+    <DevLookingGlass v-if="!isProduction" />
     <DeckDisplay />
   </div>
 </template>
