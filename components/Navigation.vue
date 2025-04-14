00139bd1178bd26b2ebbf4771a8497c226242184
Nico Diaz <nicodiazan@gmail.com>
2025-04-14 15:31:24 +0200

    Make a navigation

---
+++ components/Navigation.vue
@@ -0,0 +1,8 @@
+<template>
+  <nav>
+    <ul class="flex gap-4">
+      <li><NuxtLink to="/oracle">See</NuxtLink></li>
+      <li><NuxtLink to="/deck">The Deck</NuxtLink></li>
+    </ul>
+  </nav>
+</template>
