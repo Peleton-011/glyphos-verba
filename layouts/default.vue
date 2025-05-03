aaf5cf7d0de26ffe419ac7b93eb2b680c6fd71df
Nico Diaz <nicodiazan@gmail.com>
2025-05-03 13:59:52 +0200

    Update styling

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,6 +3,19 @@ const isProduction = process.env.NODE_ENV === "production";
 </script>
 
 <template>
+  <div class="h-screen w-screen bg-black absolute l-0 t-0 -z-10 overflow-hidden">
+    <div class="absolute -inset-[100px] opacity-50">
+      <div
+        class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-96 h-96 bg-purple-500 rounded-full mix-blend-multiply filter blur-[128px] animate-pulse"
+      ></div>
+      <div
+        class="absolute top-1/2 left-1/3 -translate-x-1/2 -translate-y-1/2 w-96 h-96 bg-cyan-500 rounded-full mix-blend-multiply filter blur-[128px] animate-pulse"
+      ></div>
+      <div
+        class="absolute top-1/2 left-2/3 -translate-x-1/2 -translate-y-1/2 w-96 h-96 bg-pink-500 rounded-full mix-blend-multiply filter blur-[128px] animate-pulse"
+      ></div>
+    </div>
+  </div>
   <header>
     <Navigation />
   </header>
@@ -10,19 +23,3 @@ const isProduction = process.env.NODE_ENV === "production";
   <hr />
   <NuxtPage />
 </template>
-
-<style>
-body {
-  /* background-color: #000; */
-  /* color: #fff; */
-  user-select: none;
-}
-
-.section-title {
-  @apply font-serif font-bold tracking-wider text-4xl p-4 w-full text-center;
-}
-
-.btn-like {
-  @apply flex z-20 text-sm bg-purple-300 text-black border rounded shadow-lg w-64 hover:cursor-pointer hover:scale-105 transition-transform;
-}
-</style>
