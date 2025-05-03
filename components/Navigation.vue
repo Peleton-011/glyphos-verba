2e4a3b24bb718bb35ecee7e940b1698db0b42202
Nico Diaz <nicodiazan@gmail.com>
2025-05-03 22:28:40 +0200

    Update the navigation to keep up with styles

--- components/Navigation.vue
+++ components/Navigation.vue
@@ -1,14 +1,15 @@
 <template>
-  <nav class="flex gap-4 p-4">
-    <NuxtLink
-      v-if="$route.path !== '/'"
-      to="/"
-      class="p-1 font-serif font-bold tracking-wider text-4xl hover:cursor-pointer lines"
-      >Glyphos Verba</NuxtLink
-    >
+  <nav class="flex justify-center items-center gap-4 p-4">
     <ul class="flex gap-4">
-      <li class="btn-like" :class="{ active: $route.path === '/oracle' }">
-        <NuxtLink to="/oracle">See the Oracle</NuxtLink>
+      <li :class="{ active: $route.path === '/oracle' }">
+        <NuxtLink to="/oracle">
+          <OutlineButton> See the Oracle </OutlineButton>
+        </NuxtLink>
+      </li>
+      <li class="flex" v-if="$route.path !== '/'">
+        <NuxtLink to="/" class="p-1 hover:cursor-pointer">
+          <Logo class="text-4xl" />
+        </NuxtLink>
       </li>
       <li class="btn-like" :class="{ active: $route.path === '/deck' }">
         <NuxtLink to="/deck">The Deck</NuxtLink>
