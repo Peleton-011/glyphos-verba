5263e51c959c81fc19fd22d2fd29766306caba77
Nico Diaz <nicodiazan@gmail.com>
2025-05-03 22:28:14 +0200

    Move over to using the Background component in the default layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,22 +1,12 @@
 <script setup lang="ts">
+import Background from "~/components/Background.vue";
+
 const isProduction = process.env.NODE_ENV === "production";
 </script>
 
 <template>
   <!-- Background -->
-  <div class="h-screen w-screen fixed bg-black l-0 t-0 -z-10 overflow-hidden">
-    <div class="absolute -inset-[100px] opacity-50">
-      <div
-        class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-96 h-96 bg-purple-500 rounded-full mix-blend-multiply filter blur-[128px] animate-pulse"
-      ></div>
-      <div
-        class="absolute top-1/2 left-1/3 -translate-x-1/2 -translate-y-1/2 w-96 h-96 bg-cyan-500 rounded-full mix-blend-multiply filter blur-[128px] animate-pulse"
-      ></div>
-      <div
-        class="absolute top-1/2 left-2/3 -translate-x-1/2 -translate-y-1/2 w-96 h-96 bg-pink-500 rounded-full mix-blend-multiply filter blur-[128px] animate-pulse"
-      ></div>
-    </div>
-  </div>
+  <Background />
   <!-- Navigation -->
   <header>
     <Navigation />
