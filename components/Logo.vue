35b61da64c70bf57d016d006363a2586a4d70869
Nico Diaz <nicodiazan@gmail.com>
2025-05-03 22:27:21 +0200

    Make a Logo component to be reused where needed

---
+++ components/Logo.vue
@@ -0,0 +1,11 @@
+<template>
+  <h1
+    class="font-serif-stretched text-glow font-bold tracking-tighter text-[#f2d16d]"
+    :class="$attrs.class"
+  >
+    ◆<span class="text-lines tracking-widest">Glyphos Verba</span>◆
+    <span
+      class="bg-gradient-to-b from-white to-slate-600 bg-clip-text text-transparent"
+    ></span>
+  </h1>
+</template>
